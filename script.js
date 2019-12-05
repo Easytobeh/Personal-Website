12727cce88b5dfad27e5a5301799020d9d134d6c
unknown <easytobeh@gmail.com>
2019-12-05 21:39:15 +0100

    Modified viewport

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#gallery").on("click", function (event) {
+        event.preventDefault();
+        alert("Content not ready. You can check back some other time" + String.fromCodePoint(0x1F615));
+    });
+
+
+
+
+});
